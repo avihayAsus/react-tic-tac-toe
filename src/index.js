e21e3e645ad678254c7f30ae216dfb947fa666e0
avihayAsusgit initgit add README.mdgit commit -m first <avihay.asus@gmail.com>
2020-04-15 12:16:58 +0300

    Saving before ejecting

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+console.log(React);
+console.log(ReactDOM);
+
+const element = (
+  <h1>
+    Hello
+  </h1>
+);
+console.log(element);
+ReactDOM.render(element,document.getElementById('root'));
